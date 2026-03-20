b09cfedccb14b2ae170c769d103af8f772d2bc83
Koppisetti Charan Sai <koppisetticharansai@gmail.com>
2026-03-20 09:53:52 +0530

    changes in html and js files

--- force-app/main/default/lwc/caseDashboard/caseDashboard.js
+++ force-app/main/default/lwc/caseDashboard/caseDashboard.js
@@ -1,7 +1,9 @@
-import { LightningElement,wire } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 import getCaseStats from '@salesforce/apex/CaseDashboardController.getCaseStats';
 import getCasesByOrigin from '@salesforce/apex/CaseDashboardController.getCasesByOrigin';
-export default class CaseDashboard extends LightningElement {
+
+export default class CaseDashboard extends NavigationMixin(LightningElement) {
 
     stats = {};
     data = [];
@@ -9,23 +11,42 @@ export default class CaseDashboard extends LightningElement {
     columns = [
         { label: 'Origin', fieldName: 'Origin' },
         { label: 'Total Cases', fieldName: 'total' }
-        
     ];
 
     @wire(getCaseStats)
-    wiredStats({data,error}){
-        if(data){
+    wiredStats({ data }) {
+        if (data) {
             this.stats = data;
         }
     }
 
     @wire(getCasesByOrigin)
-    wiredOrigin({data,error}){
-        if(data){
+    wiredOrigin({ data }) {
+        if (data) {
             this.data = data.map(row => ({
                 Origin: row.Origin,
                 total: row.total
             }));
         }
     }
+
+    handleCardClick(event) {
+    const filterName = event.currentTarget.dataset.filter;
+
+    const pageRef = {
+        type: 'standard__objectPage',
+        attributes: {
+            objectApiName: 'Case',
+            actionName: 'list'
+        },
+        state: {
+            filterName: filterName
+        }
+    };
+
+    // Generate URL and open in new tab
+    this[NavigationMixin.GenerateUrl](pageRef).then(url => {
+        window.open(url, '_blank');
+    });
+}
 }
